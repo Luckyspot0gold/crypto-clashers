000c8e31fef4db768217b28f4382d737cedb163f
Justin STYRD Gaming <mammerjam303@gmail.com>
2025-06-17 22:48:03 -0600

    Create app.js

---
+++ app/app.js
@@ -0,0 +1,4 @@
+// In your App.js or main component
+<footer style={{ position: 'absolute', bottom: 0, width: '100%', textAlign: 'center', color: '#FFD700' }}>
+  Powered by StoneVerse Protocol 7
+</footer>
