9bb03815255b2e54c4b1be89e85edbb35145458e
Justin STYRD Gaming <mammerjam303@gmail.com>
2025-06-28 21:04:33 -0600

    Create dependencsy.js

---
+++ animations_engine/dependencsy.js
@@ -0,0 +1,12 @@
+```bash
+npm install avalanche@^3.0 ethers@^6.0 @bolt-io/sdk venice-sdk
+```
+
+2. **Environment Variables** (`.env.local`):
+```env
+AVAX_NODE_URL=https://api.avax.network/ext/bc/C/rpc
+BOLT_API_KEY=your_bolt_key
+WYO_CONTRACT_ADDRESS=0xYourContract
+CRYPTO_CLASHERS_ADDRESS=0xClashersContract
+VENICE_API_KEY=your_venice_key
+```
