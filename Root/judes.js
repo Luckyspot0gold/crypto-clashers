742debcd995daa9c7c9892abf7e57432fb759bfd
Justin STYRD Gaming <mammerjam303@gmail.com>
2025-06-28 19:26:42 -0600

    Create judes.js

---
+++ Root/judes.js
@@ -0,0 +1,16 @@
+1. Open **Judge Mode**:  
+   https://cryptoclashers.games?judge=1
+
+2. Try these commands with Bar Keep Bill:  
+   - "Show me Bitcoin technicals"  
+   - "Analyze last boxing match"  
+   - "Pour me a whiskey"  
+
+3. Boxing Arena Secrets:  
+   - Type "GOLD RUSH" for special match  
+   - Click gloves 5x for developer console  
+
+4. Revenue Proof:  
+   - View /revenue-dashboard  
+   - Transaction IDs verifiable on Avalanche
+```
